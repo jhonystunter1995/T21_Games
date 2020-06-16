c53127dee490e5024e15150eb695bf2ac679fe13
Jhony Tabares <Jhonyrust96@gmail.com>
2020-06-15 23:09:53 -0500

    caja de login
    Co-Authored-By: null <jhonystunter1995@users.noreply.github.com>

--- resources/views/connect/login.blade.php
+++ resources/views/connect/login.blade.php
@@ -2,14 +2,24 @@
 @section('title', 'login')
 
 @section('content')
-  <div class="box">
+  <div class="box box_login shadow">
     {!! Form::open(['url' => '/login']) !!}
+    <label for="email">Correo Electrónico:</label>
     <div class="input-group">
       <div class="input-group-prepend">
           <div class="input-group-text"><i class="fas fa-envelope-open-text"></i></div>
       </div>
       {!! Form::email('email', null, ['class' => 'form-control']) !!}
     </div>
+
+    <label for="email" class="jump1">Contraseña:</label>
+  <div class="input-group">
+    <div class="input-group-prepend">
+      <div class="input-group-text"><i class="fas fa-key"></i></div>
+    </div>
+      {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
+    {!! Form::submit('Ingresar', ['class' => 'btn btn-primary jump1']) !!}
     {!! Form::close() !!}
   </div>
 @stop
